1b97126cd63b8bf361f02bdf083f621fd78c50e7
T. Salami <salami.tope@gmail.com>
2020-03-29 03:21:24 +0900

    Change routes

--- packages/reactbot/lib/modules/chatbot-meteor/routes.js
+++ packages/reactbot/lib/modules/chatbot-meteor/routes.js
@@ -16,21 +16,23 @@ WebApp.connectHandlers.use( bodyParser.urlencoded({ extended: true }));
 WebApp.connectHandlers.use( async (req, res, next) => {
 
     
-    // res.setHeader('Content-Type', 'application/json')
-    // if(req.url === '/'){
-    //     res.write(JSON.stringify({'hello': 'there'}))
-    //     res.end()
+    res.setHeader('Content-Type', 'application/json')
+    if(req.url === '/'){
+        res.write(JSON.stringify({'hello': 'there'}))
+        res.end()
 
-    // } else if (req.url === "/api/df_text_query" ){    
-    //     let responses = await chatbot.textQuery(req.body.text, req.body.parameters);   
-    //     res.write(JSON.stringify(responses[0].queryResult))
-    //     res.end()
+    } else if (req.url === "/api/df_text_query" ){  
+        console.log('here')  
+        let responses = await chatbot.textQuery(req.body.text, req.body.parameters);
+        console.log('responses')   
+        await res.write(JSON.stringify(responses[0].queryResult))
+        res.end()
 
-    // } else if (req.url === "/api/df_event_query" ){
-    //     let responses = await chatbot.eventQuery(req.body.event, req.body.parameters);
-    //     res.write(JSON.stringify(responses[0].queryResult))
-    //     res.end()
-    // }
+    } else if (req.url === "/api/df_event_query" ){
+        let responses = await chatbot.eventQuery(req.body.event, req.body.parameters);
+        res.write(JSON.stringify(responses[0].queryResult))
+        res.end()
+    }
    
     
 })
